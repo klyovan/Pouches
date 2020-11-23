0fa18ffa2369373aabb2c5b4923ebe1f3c1f0e01
klyovan <39466837+klyovan@users.noreply.github.com>
2020-11-23 15:46:52 +0200

    add condition

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,13 +3,15 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-        int[] pouches = {90, 100, 100, 100, 100}; //initial data
+        int[] pouches = {100, 100, 100, 100, 90}; //initial data
         int answer = 0;
         float alterAnswer = 0;
+        int incorrectPouches = 0;
 
         for (int i = 1; i <= pouches.length; i++) {
             if (pouches[i - 1] / i != 100 / i) {
                 answer = i;
+                incorrectPouches++;
                 if (pouches[i - 1] != 90) {
                     alterAnswer = pouches[i - 1];
                 }
@@ -18,8 +20,10 @@ public class Main {
 
         if (answer == 0) {
             System.out.println("Array is ideal :)");
-        } else if (alterAnswer != 0 && alterAnswer != 0) {
+        } else if (alterAnswer != 0) {
             System.out.println("Pouch with number " + answer + " has " + alterAnswer + "grams. Follow the terms of the task !");
+        } else if (incorrectPouches > 1) {
+            System.out.println("Only one puch have to be less then 100 grams");
         } else {
             System.out.println("Pouch with number " + answer + " has 90 grams");
         }
